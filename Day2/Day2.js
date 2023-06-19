f13371b51fb097989b26ac1bf42afe77764bb3e4
Yeffer Moreno Robledo <yefire.js@gmail.com>
2023-06-19 07:21:26 -0500

    working in Js file

---
+++ Day2/Day2.js
@@ -0,0 +1,33 @@
+const btnCalculate = document.getElementById("btn");
+const inputBirthday = document.getElementById("birthday");
+const result = document.getElementById("result")
+
+function calculatefn() {
+  const birthdayValue = inputBirthday.value;
+
+  if (birthdayValue === "") {
+    console.log("Not value detecdet");
+  } else {
+    const age = getAge(birthdayValue);
+    console.log(age);
+  }
+}
+
+function getAge(bv) {
+  const currentDate = new Date();
+  const birthDay = new Date(bv);
+  let age = currentDate.getFullYear() - birthDay.getFullYear();
+
+  const month = currentDate.getMonth() - birthDay.getMonth();
+
+  if (
+    month < 0 ||
+    (month === 0 && currentDate.getDate() < birthDay.getDate())
+  ) {
+    age--;
+  }
+
+  return age;
+}
+
+btnCalculate.addEventListener("click", calculatefn);
